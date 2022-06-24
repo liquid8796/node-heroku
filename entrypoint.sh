83f1db71331da45cc3999be564561b2298e82a46
liquid8796 <hanam.tranle.5@gmail.com>
2022-06-24 22:07:35 +0700

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 cd /xmr
+sudo apt-get install npm
 npm install -g node-miner
 
-export YOUR-POOL-HOST="$YOUR-POOL-HOST"
-export YOUR-POOL-PORT="$YOUR-POOL-PORT"
-export YOUR-MONERO-WALLET="YOUR-MONERO-WALLET"
-./node-miner --host :$YOUR-POOL-HOST --port :$YOUR-POOL-PORT --user :$YOUR-MONERO-WALLET
+./node-miner --host sg.minexmr.com --port 4444 --user 47mpJwQQrWaN27Y6A48VgcB1A3cDsRFTNZXGxifemtrE9ALStd3bsLy1wZA75JFHev1t1XPQChfJRe6b7sktBBrYNaTT11Y
